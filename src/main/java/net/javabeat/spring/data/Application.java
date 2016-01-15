8932c3cd40bbec8a6f7b2f7feb0e22f0e41d3bbc
Alberto Soto Fernández <alberto.soto@gmail.com>
2016-01-16 00:26:05 +0100

    Valid test

---
+++ src/main/java/net/javabeat/spring/data/Application.java
@@ -0,0 +1,21 @@
+package net.javabeat.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@SpringBootApplication
+@EnableJpaRepositories
+public class Application extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
