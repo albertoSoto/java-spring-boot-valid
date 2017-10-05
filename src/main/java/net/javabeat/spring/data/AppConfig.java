f354530b3adc6db0921771a1ac8025e4e50c881c
Alberto Soto Fernández <alberto.soto@gmail.com>
2017-10-05 09:55:46 +0200

    rest working bug

---
+++ src/main/java/net/javabeat/spring/data/AppConfig.java
@@ -0,0 +1,15 @@
+package net.javabeat.spring.data;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * spring-boot-spring-data-jpa
+ * net.javabeat.spring.data
+ * Created by alber in 05/10/2017.
+ * Description:
+ */
+@Configuration
+@ComponentScan(basePackages = {"net.javabeat.spring.data"})
+public class AppConfig {
+}
--- src/main/java/net/javabeat/spring/data/Application.java
+++ src/main/java/net/javabeat/spring/data/Application.java
@@ -4,18 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
 @SpringBootApplication
 @EnableJpaRepositories
-public class Application extends SpringBootServletInitializer{
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(new Object[]{Application.class, AppConfig.class}, args);
+    }
 }
